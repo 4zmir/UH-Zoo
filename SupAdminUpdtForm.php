a779d19efe46426eb41a06cc4c10fbd99707e24f
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2020-04-10 15:20:41 -0500

    Update SupAdminUpdtForm.php

--- SupAdminUpdtForm.php
+++ SupAdminUpdtForm.php
@@ -22,8 +22,10 @@ if(isset($_GET['id']) && ($_GET['id']!== '')){
 	$product_id = $_GET['id'];
 
 
-	$sql="SELECT * from user
-		LEFT JOIN department ON 
+	$sql="SELECT user.*, d.department_name
+        FROM user
+		LEFT JOIN department as d ON 
+        user.department_id = d.department_id
 		where user_id = '$product_id'";
 	$db->query($sql);
 	$item = $db->single();
@@ -31,17 +33,17 @@ if(isset($_GET['id']) && ($_GET['id']!== '')){
 }
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-	$DOB = $_POST['DOB'];
-    $gender = $_POST['gender'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $user_fname = $_POST['user_fname'];
+    $user_lname = $_POST['user_lname'];
+	$user_DOB = $_POST['user_DOB'];
+    $user_gender = $_POST['user_gender'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
 	$department_id = $_POST['department_id'];
     
     $sql= "
-    UPDATE user SET user_fname = '$fname', user_lname = '$lname', user_DOB = '$DOB', user_gender = '$gender', user_email = '$email', 
-    user_password = '$password', department_id = '$department_id'
+    UPDATE user SET user_fname = '$user_fname', user_lname = '$user_lname', user_DOB = '$user_DOB', user_gender = '$user_gender', user_email = '$user_email', 
+    user_password = '$user_password', department_id = '$department_id'
     WHERE user_id = '$product_id'";
     $db->query($sql);
 	$db->execute();
@@ -55,16 +57,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Update Ride</title>
-  <link rel="stylesheet" href="product.css">
-  <link rel="stylesheet" href="sidebar.css">
+    <title>Update Ride</title>
+    <link rel="stylesheet" href="product.css">
+    <link rel="stylesheet" href="sidebar.css">
 </head>
 
 <body>
 
-  <div id="sidebar">
+    <div id="sidebar">
     <div class="toggle-btn" onclick="toggleSidebar()">
         <span></span>
         <span></span>
@@ -73,59 +75,59 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     </div>
     <ul class="side-ul">
         <li class="side-li"><a class="side" href="supadmin_menu.php">Dashboard</a></li>
-		<li class="side-li"><a class="side" href="supAdminEmpInput.php">Input New Employee</a></li>
+        <li class="side-li"><a class="side" href="supAdminEmpInput.php">Input New Employee</a></li>
         <li class="side-li"><a class="side" href="supAdminEmpList.php">See All Employees</a></li>
         <li class="side-li"><a class="side" href="logoutScript.php">Logout</a></li>
     </ul>
-  </div>
+    </div>
 
-  <header id="imgcontainer"></header>
-   <script src="sidebar.js"></script>
+    <header id="imgcontainer"></header>
+    <script src="sidebar.js"></script>
 
-   </body>
+    </body>
 
 
-   <form  method="post">
-		<div id="container" style='margin-bottom:6em;text-align:center;'>
-			<h1> Update Form For an Employee</h1>
-				<label for="user_fname"><br>First Name:</br></label>
-                <input type="text"  name="fname" value="<?php echo $item->user_fname; ?>" required ><br>
-                
-                <label for="user_lname"><br>Last Name:</br></label>
-                <input type="text"  name="lname" value="<?php echo $item->user_lname; ?>" required ><br>
+    <form  method="post">
+        <div id="container" style='margin-bottom:6em;text-align:center;'>
+            <h1> Update Form For an Employee</h1>
+            <label for="user_fname"><br>First Name:</br></label>
+            <input type="text"  name="user_fname" value="<?php echo $item->user_fname; ?>" required ><br>
+            
+            <label for="user_lname"><br>Last Name:</br></label>
+            <input type="text"  name="user_lname" value="<?php echo $item->user_lname; ?>" required ><br>
 
-				<label for="user_DOB"><br>Date of Birth:</br></label>
-                <input type="date"  name="DOB" value="<?php echo $item->user_DOB; ?>" required ><br>
+            <label for="user_DOB"><br>Date of Birth:</br></label>
+            <input type="date"  name="user_DOB" value="<?php echo $item->user_DOB; ?>" required ><br>
 
-                <?php $genderStatus = $item->user_gender;?>
-				<label for="user_gender"><br>Gender:</br></label>
-				<select  name="gender" required>
-				    <option value= "" <?php if($genderStatus == "") echo "SELECTED";?> >N/A</option>
-				    <option value= "Male" <?php if($genderStatus == "Male") echo "SELECTED";?> >Male</option>
-				    <option value= "Female" <?php if($genderStatus == "Female") echo "SELECTED";?> >Female</option>
-				</select><br>
+            
+            <label for="user_gender"><br>Gender:</br></label>
+            <select  name="user_gender" required>
+                <option value= "<?php echo $item->user_gender;?>"><?php echo $item->user_gender;?></option>
+                <option value= "Male">Male</option>
+                <option value= "Female">Female</option>
+            </select><br>
 
-				<label for="user_email"><br>Email:</br></label>
-                <input type="text"  name="email" value="<?php echo $item->user_email; ?>" required ><br>
+            <label for="user_email"><br>Email:</br></label>
+            <input type="text"  name="user_email" value="<?php echo $item->user_email; ?>" required ><br>
 
-                <label for="user_password"><br>Password:</br></label>
-                <input type="text"  name="password" value="<?php echo $item->user_password; ?>" required ><br>
+            <label for="user_password"><br>Password:</br></label>
+            <input type="text"  name="user_password" value="<?php echo $item->user_password; ?>" required ><br>
 
-                
-				<label for="department_id"><br>Department</br></label>
-				<select name="department_id" required>
+            
+            <label for="department_id"><br>Department</br></label>
+            <select name="department_id" required>
 
-                <option value="<?php echo $item->department_id; ?>" selected >--Select Department--</option>
-				    <?PHP 
-					foreach($department as $dp){
-					echo "<option value='$dp->department_id'>$dp->department_name </option>";
-					}							
-					?>
+            <option value="<?php echo $item->department_id; ?>" selected><?php echo $item->department_name; ?></option>
+                <?PHP 
+                foreach($department as $dp){
+                echo "<option value='$dp->department_id'>$dp->department_name </option>";
+                }							
+                ?>
 
-				</select><br>
+            </select><br>
 
-			<button class="cancel" type="button" onclick="location.href='superadmin_menu.php'">Cancel</button >
-			<button class="button" type="submit">Submit</button >
+            <button class="cancel" type="button" onclick="location.href='superadmin_menu.php'">Cancel</button >
+            <button class="button" type="submit">Submit</button >
         </div>
     </form>
 
