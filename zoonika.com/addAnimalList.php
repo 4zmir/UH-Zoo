6fc7cc499cf9e669d35f2e7fb2580728d8eb068d
IstvanW <55334150+IstvanW@users.noreply.github.com>
2020-04-11 21:21:21 -0500

    Add files via upload

--- zoonika.com/addAnimalList.php
+++ zoonika.com/addAnimalList.php
@@ -63,6 +63,7 @@ if ($_COOKIE['user_id']){
             <li class="side-li"><a class="side" href="addAnimalMenu.php">Dashboard</a></li>
             <li class="side-li"><a class="side" href="addAnimal.php">Add New Animal</a></li>
 	    <li class="side-li"><a class="side" href="animalUpdate.php">Update an Animal</a></li>
+	    <li class="side-li"><a class="side" href="animalReport.php">Animal Reports</a></li>
             <li class="side-li"><a class="side" href="logoutScript.php">Logout</a></li>
             
         </ul>
